8e849fed1875800aaf3167318ed9ab98ba4edf77
Malik Turnbull <turnbull.m29@gmail.com>
2020-07-17 18:38:26 -0400

    [still workin]

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ const homewtown
 //boolean
 //freestyle
 
-function pizza(toppings) {
-    return toppings;
+function printingredients(toppings) {
+    console.log(toppings);
 }
-function mynumber(29,39) {
-    return 29 * 39;
+function printNewnumbers(mynumber) {
+    console.log(mynumber)
 }
 
 function newNumber( [1, 2, 3]){
@@ -38,3 +38,43 @@ function newNumber( [1, 2, 3]){
 function secondRotation(sec) {
     let degrotation = 5 *20
 }
+
+function doesthisreallywork(boolean){
+    if (boolean) {
+    console.log('this works')
+    }  else {
+    console.log('This does not work')
+    }
+}
+
+const sectionOne = document.getElementById('section-one');
+console.log(sectionOne);
+const sectionTwo = document.getElementById('section-two');
+console.log(sectionTwo);
+const sectionThree = document.querySelector('section-three');
+console.log(sectionThree);
+const sectionFour=document.querySelector('sectionFour');
+console.log(sectionFour);
+
+//text content
+
+sectionOne.textContent = 'apple';
+sectionTwo.textContent = 'strawberry';
+sectionThree.textContent = 'mango';
+sectionFour.textContent = 'pineapple';
+
+const paraOne = document.querySelector('.paragraph');
+console.log(paraOne);
+paraOne.textContent = 'lemon';
+
+const paraTwo = document.querySelector('.paragraph-two');
+console.log(paraTwo);
+paraTwo.textContent = 'kiwi';
+
+const paraThree = document.querySelector('.paragraph-three');
+console.log(paraThree);
+paraThree.textContent = 'cherry';
+
+const paraFour = document.querySelector('.paragraph-four')
+console.log(paraFour);
+paraFour.textContent = 'coconut';
