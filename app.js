9974972d78720a6b243d9f7f8d55a63ada999739
Malik Turnbull <Malik@maliks-mbp.home>
2020-07-17 17:09:37 -0400

    [updated 5 variables]

---
+++ app.js
@@ -0,0 +1,17 @@
+// 5 variables
+// string
+// number 
+// array 
+// boolean
+//object (4 different key value pairs) (string, number, array, booelean)
+
+let fruit = 'mango';
+console.log(fruit);
+
+let favoriteNumber = 29;
+console.log(favoriteNumber);
+
+const fruits = ['apples', 'strawberry', 'mango', 'pineapple']
+console.log(fruits);
+
+const homewtown
