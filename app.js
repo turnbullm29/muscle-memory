a2745d275f2654015ee9143d7bb6812f89f3f753
Malik Turnbull <turnbull.m29@gmail.com>
2020-07-17 17:41:24 -0400

    [updated word]

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ const fruits = ['apples', 'strawberry', 'mango', 'pineapple']
 console.log(fruits);
 
 const homewtown
+
+// 10 different functions
+//pass array
+//pass in a callback 
+//string
+//object
+//boolean
+//freestyle
+
+function pizza(toppings) {
+    return toppings;
+}
+function mynumber(29,39) {
+    return 29 * 39;
+}
+
+function newNumber( [1, 2, 3]){
+   return 1+2+3; 
+}
+
+function secondRotation(sec) {
+    let degrotation = 5 *20
+}
